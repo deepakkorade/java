44aa8257fe583245bf6885dd2e0cf20b23aac129
deepak korade <147805353+deepakkorade@users.noreply.github.com>
2024-08-15 20:16:23 +0530

    factorial

---
+++ logicalprogram/factorial.java
@@ -0,0 +1,15 @@
+package logicalprogram;
+
+public class factorial {
+    public static void main(String[] arg){
+        int n=4;
+        int fact=1;
+        for(int i=1;i<=n;i++){
+            
+           fact=fact*i;
+        }
+            System.out.println(fact);
+        
+    }
+    
+}
--- patternsprinting/simple.java
+++ patternsprinting/simple.java
@@ -1,1 +1,17 @@
+package patternsprinting;
 
+import java.util.Scanner;
+
+public class simple {
+     public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
