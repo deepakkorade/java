d2c225d4247f67a1030ba9d927f43bf0ba935776
deepak korade <147805353+deepakkorade@users.noreply.github.com>
2024-08-17 14:56:15 +0530

    factorial.java

--- logicalprogram/factorial.java
+++ logicalprogram/factorial.java
@@ -1,15 +1,10 @@
 package logicalprogram;
-
 public class factorial {
     public static void main(String[] arg){
-        int n=4;
         int fact=1;
-        for(int i=1;i<=n;i++){
-            
-           fact=fact*i;
+        for(int i=1;i<=4;i++){
+          fact*=i;
         }
             System.out.println(fact);
-        
     }
-    
 }
