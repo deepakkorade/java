2408f0ed5913f278c3248ea91c8b77d72d607794
deepak korade <147805353+deepakkorade@users.noreply.github.com>
2024-08-22 21:44:35 +0530

    palindromno.java

---
+++ logicalprogram/palindromno.java
@@ -0,0 +1,21 @@
+package logicalprogram;
+
+public class palindromno {
+    public static void main(String[] arg){
+        int r,sum=0,temp;    
+  int n=454;
+
+  temp=n;    
+  while(n>0){    
+   r=n%10;    
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }    
+  if(temp==sum)    
+   System.out.println("palindrome number ");    
+  else    
+   System.out.println("not palindrome");    
+}  
+    }
+    
+
