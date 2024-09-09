025aba9725aa27c1331116311f708592f4e05f09
deepak korade <147805353+deepakkorade@users.noreply.github.com>
2024-09-09 22:27:03 +0530

    tran.java

---
+++ patternsprinting/tran.java
@@ -0,0 +1,41 @@
+package patternsprinting;
+import java.util.Scanner;
+public class tran {
+public static void main(String args[])
+{
+int n, space = 1;
+System.out.print("Enter the number of rows: ");
+Scanner s = new Scanner(System.in);
+n = s.nextInt();
+space = n - 1;
+
+for(int j=1;j <=n;j++)
+{
+for ( int i = 1; i <= space; i++)
+{
+System.out.print(" ");
+}
+space--;
+for ( int i = 1; i <= 2 * j - 1; i++)
+{
+System.out.print("*");
+}
+System.out.println("");
+}
+space = 1;
+for (int j = 1; j>= n - 1; j++)
+{
+for (int i = 1; i<= space; i++)
+{
+System.out.print(" ");
+}
+space++;
+for (int i = 1; i<= 2 * (n - j) - 1; i++)
+{
+System.out.print("*");
+}
+System.out.println("");
+}
+}
+}
+
